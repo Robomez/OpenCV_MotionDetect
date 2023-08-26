b41f9d2f39da032859104dc98555780fa31ef856
Robomez-HP <alextimofee@gmail.com>
2023-08-26 17:52:51 +0300

    Изображение с камеры на экран в реальном времени. Ориентация фиксирована на горизонтальную в манифесте.

--- app/src/main/java/com/example/opencv_motiondetect/MainActivity.java
+++ app/src/main/java/com/example/opencv_motiondetect/MainActivity.java
@@ -1,100 +1,89 @@
 package com.example.opencv_motiondetect;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.graphics.Bitmap;
+import android.content.pm.PackageManager;
+import android.Manifest;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
 import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
 
-import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends CameraActivity {
 
-    private static final int SELECT_CODE = 1, CAMERA_CODE = 2;
-    Button select, camera;
-    ImageView imageView;
-    Bitmap bitmap;
-    Mat mat;
+    CameraBridgeViewBase cameraBridgeViewBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getPermission();
+
+        cameraBridgeViewBase = findViewById(R.id.cameraView);
+        cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
+            @Override
+            public void onCameraViewStarted(int width, int height) {
+
+            }
+
+            @Override
+            public void onCameraViewStopped() {
+
+            }
+
+            // When a frame is captured by android camera
+            @Override
+            public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+                // cast input frame to Mat
+                return inputFrame.rgba();
+            }
+        });
+
         if (OpenCVLoader.initDebug()) {
             Log.d("OPENCVLOAD", "success");
         } else {
             Log.d("OPENCVLOAD", "error");
         }
-
-        camera = findViewById(R.id.camera);
-        select = findViewById(R.id.select);
-        imageView = findViewById(R.id.imageView);
-
-        select.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("image/*");
-            startActivityForResult(intent, SELECT_CODE);
-        });
-
-        camera.setOnClickListener(v -> {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent, CAMERA_CODE);
-        });
+        cameraBridgeViewBase.enableView();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == SELECT_CODE && data != null) {
-            try {
-                // Получили bitmap
-                bitmap = MediaStore
-                        .Images
-                        .Media
-                        .getBitmap(this.getContentResolver(), data.getData());
-                // Установили содержимое imageView этот bitmap
-                imageView.setImageBitmap(bitmap);
-                // В матрицу openCV загрузили bitmap
-                mat = new Mat();
-                Utils.bitmapToMat(bitmap, mat);
+    protected void onResume() {
+        super.onResume();
+        cameraBridgeViewBase.enableView();
+    }
 
-                // Convert to grayscale
-                // Функция openCV для конвертации в gc
-                Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
-                // Так же из матрицы конвертированных значений перевести в bitmap
-                Utils.matToBitmap(mat, bitmap);
-                // Установить в imageView
-                imageView.setImageBitmap(bitmap);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraBridgeViewBase.disableView();
+    }
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+    // В современных версиях Android нет нужды проверять, он сам проверяет
+    void getPermission() {
+        if (checkSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 3);
         }
-        if (requestCode == CAMERA_CODE && data != null) {
-            bitmap = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(bitmap);
-            mat = new Mat();
-            Utils.bitmapToMat(bitmap, mat);
-
-            // Convert to grayscale
-            // Функция openCV для конвертации в gc
-            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2BGR);
-            // Так же из матрицы конвертированных значений перевести в bitmap
-            Utils.matToBitmap(mat, bitmap);
-            // Установить в imageView
-            imageView.setImageBitmap(bitmap);
+    }
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            String[] permissions,
+            int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            getPermission();
         }
+    }
+
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(cameraBridgeViewBase);
     }
 }

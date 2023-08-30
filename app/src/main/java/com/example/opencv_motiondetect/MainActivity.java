98d575c63b7cc8d004e24cbf3a6d8f55d558c430
Robomez-HP <alextimofee@gmail.com>
2023-08-30 10:45:21 +0300

    Выделение движущихся объектов прямоугольником. Для конкретной реализации требуется точная настройка.

--- app/src/main/java/com/example/opencv_motiondetect/MainActivity.java
+++ app/src/main/java/com/example/opencv_motiondetect/MainActivity.java
@@ -9,16 +9,26 @@ import org.opencv.android.CameraActivity;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.video.BackgroundSubtractorKNN;
 import org.opencv.video.BackgroundSubtractorMOG2;
 import org.opencv.video.Video;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends CameraActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     BackgroundSubtractorMOG2 backSub;
+    BackgroundSubtractorKNN backSubKNN;
     JavaCameraView javaCameraView;
-    Mat frame, subMask;
+    Mat frame, subMask, frameResult;
+    List<MatOfPoint> contours;
 
     BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(MainActivity.this) {
         @Override
@@ -57,6 +67,10 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
         if (javaCameraView != null) {
             javaCameraView.disableView();
         }
+        frame.release();
+        subMask.release();
+        backSub.clear();
+        backSubKNN.clear();
     }
 
     // В современных версиях Android нет нужды проверять, он сам проверяет
@@ -81,8 +95,11 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
     @Override
     public void onCameraViewStarted(int width, int height) {
         frame = new Mat();
+        frameResult = new Mat();
         subMask = new Mat();
         backSub = Video.createBackgroundSubtractorMOG2();
+        backSubKNN = Video.createBackgroundSubtractorKNN();
+        contours = new ArrayList<>();
     }
 
     @Override
@@ -92,11 +109,45 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        frame = inputFrame.gray().t();
-        Core.flip(frame, frame, 1);
-        backSub.apply(frame, subMask);
 
-        return subMask;
+        frame = inputFrame.gray();
+        frameResult = inputFrame.rgba();
+
+        /*
+        // При вертикальной ориентации
+        Core.transpose(frame, frame);
+        Core.flip(frame, frame, 1);
+        */
+
+        Imgproc.GaussianBlur(frame, frame, new Size(3,3), 0, 0);
+        // backSub.apply(frame, subMask);
+        backSubKNN.apply(frame, subMask);
+
+        Imgproc.erode(subMask,
+                subMask,
+                Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3,3))
+        );
+        Imgproc.dilate(
+                subMask,
+                subMask,
+                Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3))
+        );
+        Imgproc.findContours(
+                subMask,
+                contours,
+                new Mat(),
+                Imgproc.RETR_EXTERNAL,
+                Imgproc.CHAIN_APPROX_SIMPLE
+        );
+
+        for (MatOfPoint m : contours) {
+            Rect rectangle = Imgproc.boundingRect(m);
+            Imgproc.rectangle(frameResult, rectangle, new Scalar(255, 255, 0), 4);
+        }
+        contours.clear();
+
+        // return frame;
+        return frameResult;
     }
 
     @Override
